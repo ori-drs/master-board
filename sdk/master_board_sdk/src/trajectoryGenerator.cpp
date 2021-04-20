e65eb37f426459801e198e6ad3b10afbb6681340
ferdinand-wood <ferdinand.elhardt@live.de>
2021-04-20 15:46:27 +0100

    minor changes

--- sdk/master_board_sdk/src/trajectoryGenerator.cpp
+++ sdk/master_board_sdk/src/trajectoryGenerator.cpp
@@ -10,12 +10,26 @@
 #include <math.h>
 #include "master_board_sdk/trajectoryGenerator.h"
 
-double TrajectoryGenerator::sinePos(double init_pos, TrajectoryParameters para, double t)
+double TrajectoryGenerator::sinePos(TrajectoryParameters para, double t)
 {
-    return init_pos + para.amplitude * sin((2 * M_PI * para.frequency * t)+para.phaseshift);
+    if (para.active)
+        return para.zero_pos + para.start_pos + para.amplitude * sin((2 * M_PI * this->frequency(para, t) * t) + para.phaseshift);
+    else
+        return para.zero_pos;
 }
 
-double TrajectoryGenerator::sineVel(double init_pos, TrajectoryParameters para, double t)
+double TrajectoryGenerator::sineVel(TrajectoryParameters para, double t)
 {
-    return 2. * M_PI * para.frequency * para.amplitude * cos((2 * M_PI * para.frequency * t)+para.phaseshift);
+    if (para.active)
+        return 2. * M_PI * this->frequency(para, t) * para.amplitude * cos((2 * M_PI * this->frequency(para, t) * t) + para.phaseshift);
+    else
+        return 0;
+}
+
+double TrajectoryGenerator::frequency(TrajectoryParameters para, double t)
+{
+    double frequency_ = para.frequencyStart + para.frequencyInclination*t;
+    if (frequency_ <= para.frequencyEnd)
+        frequency_ = para.frequencyEnd;
+    return frequency_;
 }
