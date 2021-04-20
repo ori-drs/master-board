4bd984c8b39526f2c82cbf8d05736c1e5975ff46
ferdinand-wood <ferdinand.elhardt@live.de>
2021-04-20 15:46:05 +0100

    minor changes

--- sdk/master_board_sdk/include/master_board_sdk/trajectoryGenerator.h
+++ sdk/master_board_sdk/include/master_board_sdk/trajectoryGenerator.h
@@ -4,15 +4,22 @@
 struct TrajectoryParameters
 {
     bool active = 0;
-    double frequency = 0.6;
+    double frequencyStart = 0.6;
+    double frequencyInclination = 0.0;
+    double frequencyEnd = 0.6;
     double amplitude = 0;
     double phaseshift = 0;  // in rad
+    double init_pos = 0;    // value where the sine curve will cycle around
+    double zero_pos = 0;    // starting value when robot is startet (should be zero normally because of encoder reset)
+    double startupPosition = 0;    // starting value when robot is startet (should be zero normally because of encoder reset)
+    double start_pos = 0;
 };
 
 class TrajectoryGenerator
 {
 public:
-    double sinePos(double init_pos, TrajectoryParameters para, double t);
-    double sineVel(double init_pos, TrajectoryParameters para, double t);
+    double sinePos(TrajectoryParameters para, double t);
+    double sineVel(TrajectoryParameters para, double t);
+    double frequency(TrajectoryParameters para, double t);
 };
 
